c01a0e87c6f71a470fdeb44c80e28666361e6107
Niels Kristian Lyshøj Jensen <n@nk.je>
2020-12-15 19:52:38 +0100

    Add hydraJobs for x86_64-linux

--- flake.nix
+++ flake.nix
@@ -7,10 +7,17 @@
     };
   };
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "nixscopy";
-      overlay = ./overlay.nix;
-      systems = flake-utils.lib.defaultSystems;
+    let
+      flake = flake-utils.lib.simpleFlake {
+        inherit self nixpkgs;
+        name = "nixscopy";
+        overlay = ./overlay.nix;
+        systems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+      };
+    in
+    flake // {
+      hydraJobs = {
+        x86_64-linux = flake.packages.x86_64-linux;
+      };
     };
 }
